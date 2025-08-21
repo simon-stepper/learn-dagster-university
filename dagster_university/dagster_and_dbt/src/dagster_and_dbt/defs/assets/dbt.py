73b4c7d237f5d8a3dfa5311880838ef4e7ca8884
Simon Stepper <simon.stepper@lanes-planes.com>
2025-08-21 17:39:26 +0200

    dbt lesson 5

--- dagster_university/dagster_and_dbt/src/dagster_and_dbt/defs/assets/dbt.py
+++ dagster_university/dagster_and_dbt/src/dagster_and_dbt/defs/assets/dbt.py
@@ -1,10 +1,24 @@
 import dagster as dg
-from dagster_dbt import dbt_assets, DbtCliResource
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
+from dagster import AssetKey
 
 from dagster_and_dbt.defs.project import dbt_project
 
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        if resource_type == "source":
+            return dg.AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+    def get_group_name(self, dbt_resource_props):
+        return dbt_resource_props["fqn"][1]
+
+
 @dbt_assets(
     manifest=dbt_project.manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
 )
 def dbt_analytics(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
